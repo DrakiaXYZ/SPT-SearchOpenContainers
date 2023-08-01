5ffe7ab11e1909943062f8401e03bfc002d9adf5
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-07-31 21:41:53 -0700

    Even more future proof, no more obfuscated method name access, everything is done by parameter names instead, which should be more static
    Also properly localize the "Search" text

--- Patches/ContainerMenuPatch.cs
+++ Patches/ContainerMenuPatch.cs
@@ -17,6 +17,7 @@ namespace DrakiaXYZ.SearchOpenContainers.Patches
         private static Type _menuClassType;
         private static Type _menuItemClassType;
         private static Type _interactionTypeClassType;
+        private static Type _stringLocalizeType;
 
         private static FieldInfo _menuItemNameField;
         private static FieldInfo _menuItemActionField;
@@ -24,6 +25,7 @@ namespace DrakiaXYZ.SearchOpenContainers.Patches
 
         private static MethodInfo _containerSearchMethod;
         private static MethodInfo _playerInteractContainerMethod;
+        private static MethodInfo _localizedMethod;
 
         private static object interactionTypeInstance;
 
@@ -34,6 +36,7 @@ namespace DrakiaXYZ.SearchOpenContainers.Patches
             _menuClassType = PatchConstants.EftTypes.Single(x => x.GetMethod("SelectNextAction") != null); // GClass2888
             _menuItemClassType = PatchConstants.EftTypes.Single(x => x.GetField("TargetName") != null && x.GetField("Disabled") != null); // GClass2887
             _interactionTypeClassType = PatchConstants.EftTypes.Single(x => x.GetField("InteractionType") != null && x.BaseType == typeof(object)); // GClass2843
+            _stringLocalizeType = PatchConstants.EftTypes.Single(x => x.GetMethod("LocalizeAreaName") != null);
 
             // Get field accessors we'll need
             _menuItemNameField = AccessTools.Field(_menuItemClassType, "Name");
@@ -41,13 +44,36 @@ namespace DrakiaXYZ.SearchOpenContainers.Patches
             _menuActionsField = AccessTools.Field(_menuClassType, "Actions");
 
             // Get method info we'll need
-            _containerSearchMethod = AccessTools.Method(_playerActionClassType, "smethod_17");
-            _playerInteractContainerMethod = AccessTools.Method(typeof(Player), "vmethod_1");
+            _containerSearchMethod = AccessTools.GetDeclaredMethods(_playerActionClassType).FirstOrDefault(mi =>
+            {
+                var parameters = mi.GetParameters();
+                return parameters.Length == 4
+                    && parameters[0].Name == "owner"
+                    && parameters[1].Name == "callback"
+                    && parameters[2].Name == "lootableContainer"
+                    && parameters[3].Name == "initialDistance";
+            });
+
+            _playerInteractContainerMethod = AccessTools.GetDeclaredMethods(typeof(Player)).FirstOrDefault(mi =>
+            {
+                var parameters = mi.GetParameters();
+                return parameters.Length == 2
+                    && parameters[0].Name == "door"
+                    && parameters[1].Name == "interactionResult";
+            });
+
+            _localizedMethod = AccessTools.Method(_stringLocalizeType, "Localized", new Type[] { typeof(string), typeof(string) });
 
             // We only need one instance of the InteractionType class, so create it now
             interactionTypeInstance = Activator.CreateInstance(_interactionTypeClassType, new object[] { EInteractionType.Open });
 
-            return AccessTools.Method(_playerActionClassType, "smethod_11");
+            return AccessTools.GetDeclaredMethods(_playerActionClassType).FirstOrDefault(mi =>
+            {
+                var parameters = mi.GetParameters();
+                return parameters.Length == 2
+                    && parameters[0].Name == "owner"
+                    && parameters[1].Name == "container";
+            });
         }
 
         [PatchPostfix]
@@ -70,7 +96,7 @@ namespace DrakiaXYZ.SearchOpenContainers.Patches
                 object searchMenuItem = Activator.CreateInstance(_menuItemClassType, new object[] { });
 
                 // And then use FieldInfo objects to populate its
-                _menuItemNameField.SetValue(searchMenuItem, "Search");
+                _menuItemNameField.SetValue(searchMenuItem, _localizedMethod.Invoke(null, new object[] { "Search", null }));
                 _menuItemActionField.SetValue(searchMenuItem, new Action(actionHandler.StartOpenContainer));
 
                 // And because our list is generic, we can just insert into it like normal
