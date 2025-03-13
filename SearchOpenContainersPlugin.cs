1466f1ff42a41594f77f6dd7a17ec2b7a0aff744
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-03-12 17:07:51 -0700

    Update for 3.11

--- SearchOpenContainersPlugin.cs
+++ SearchOpenContainersPlugin.cs
@@ -3,17 +3,13 @@ using DrakiaXYZ.SearchOpenContainers.Patches;
 
 namespace DrakiaXYZ.SearchOpenContainers
 {
-    [BepInPlugin("xyz.drakia.searchopencontainers", "DrakiaXYZ-SearchOpenContainers", "1.2.0")]
-    [BepInDependency("com.SPT.core", "3.10.0")]
+    [BepInPlugin("xyz.drakia.searchopencontainers", "DrakiaXYZ-SearchOpenContainers", "1.3.0")]
+    [BepInDependency("com.SPT.core", "3.11.0")]
     public class SearchOpenContainersPlugin : BaseUnityPlugin
     {
         private void Awake()
         {
-            Logger.LogInfo("[SearchOpenContainers] Loading...");
-
             new ContainerMenuPatch().Enable();
-
-            Logger.LogInfo("[SearchOpenContainers] Loaded!");
         }
     }
 }
