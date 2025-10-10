282f77a1c8b7c89e09784d89d94d9056798c97f9
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-10-09 19:20:27 -0700

    Update for 4.0.0 Switch to netstandard2.1

--- SearchOpenContainersPlugin.cs
+++ SearchOpenContainersPlugin.cs
@@ -3,8 +3,8 @@ using DrakiaXYZ.SearchOpenContainers.Patches;
 
 namespace DrakiaXYZ.SearchOpenContainers
 {
-    [BepInPlugin("xyz.drakia.searchopencontainers", "DrakiaXYZ-SearchOpenContainers", "1.3.0")]
-    [BepInDependency("com.SPT.core", "3.11.0")]
+    [BepInPlugin("xyz.drakia.searchopencontainers", "DrakiaXYZ-SearchOpenContainers", "1.4.0")]
+    [BepInDependency("com.SPT.core", "4.0.0")]
     public class SearchOpenContainersPlugin : BaseUnityPlugin
     {
         private void Awake()
